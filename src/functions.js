3b45fe05ab7cbef68099eca89e1b4a49ff52e957
Pedro Alves <pedrost00700@gmail.com>
2023-11-21 17:51:55 -0300

    initial commit

---
+++ src/functions.js
@@ -0,0 +1,14 @@
+"use strict";
+// Função de multiplicação
+function multiplicacao(a, b) {
+    return a * b;
+}
+// Função de saudação
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+// Exemplos de uso:
+const resultadoMultiplicacao = multiplicacao(5, 3);
+console.log("Resultado da multiplicação:", resultadoMultiplicacao);
+const saudacaoParaUsuario = saudacao("João");
+console.log(saudacaoParaUsuario);
---
+++ src/functions.ts
@@ -0,0 +1,16 @@
+// Função de multiplicação
+function multiplicacao(a: number, b: number): number {
+    return a * b;
+}
+
+// Função de saudação
+function saudacao(nome: string): string {
+    return "Olá " + nome;
+}
+
+// Exemplos de uso:
+const resultadoMultiplicacao = multiplicacao(5, 3);
+console.log("Resultado da multiplicação:", resultadoMultiplicacao);
+
+const saudacaoParaUsuario = saudacao("João");
+console.log(saudacaoParaUsuario);
